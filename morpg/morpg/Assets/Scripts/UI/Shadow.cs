1fdd77af1f8dc48ab8b9a4ba4355e8fc686ea079
NDDYLYS <nodvic@naver.com>
2026-02-27 20:58:54 +0900

    add tmp

--- morpg/morpg/Assets/Scripts/UI/Shadow.cs
+++ morpg/morpg/Assets/Scripts/UI/Shadow.cs
@@ -1,45 +1,45 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 
 
 
 public class Shadow : MonoBehaviour
 {
     public GameObject Image;
-    public Text Text;
+    public TextMeshProUGUI Text;
 
     void Awake()
     {
-        //PlayShadow();
+        PlayShadow();
     }
 
-    //public void PlayShadow()
-    //{
-    //    StartCoroutine("ShadowCoroutine");
-    //}
-    
-    //private IEnumerator ShadowCoroutine()
-    //{
-    //    int chapter = (GameManager.Instance.SaveData.InstantDungeon == null) ? 0 : GameManager.Instance.SaveData.InstantDungeon.Chapter;
-    //    Image.SetActive(true);
-    //    Text.gameObject.SetActive(true);
-    //    Text.text = string.Empty;
+    public void PlayShadow()
+    {
+        StartCoroutine("ShadowCoroutine");
+    }
 
-    //    Table_Map map = TableDataManager.Instance.GetMapForChater(chapter);
-    //    yield return new WaitForSeconds(2f);
-    //    Text.text = TableDataManager.Instance.GetTableText(map.Name);
+    private IEnumerator ShadowCoroutine()
+    {
+        //int chapter = (GameManager.Instance.SaveData.InstantDungeon == null) ? 0 : GameManager.Instance.SaveData.InstantDungeon.Chapter;
+        //Image.SetActive(true);
+        //Text.gameObject.SetActive(true);
+        //Text.text = string.Empty;
 
-    //    yield return new WaitForSeconds(1.5f);
+        //Table_Map map = TableDataManager.Instance.GetMapForChater(chapter);
+        //yield return new WaitForSeconds(2f);
+        //Text.text = TableDataManager.Instance.GetTableText(map.Name);
 
-    //    Text.text = string.Empty;
-    //    Text.gameObject.SetActive(false);
-    //    Image.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
 
-    //    GameManager.Instance.OccurEvent(EventKind.MonsterAppear);
-    //    GameManager.Instance.OccurEvent(EventKind.PlayTimeStart);
+        //Text.text = string.Empty;
+        //Text.gameObject.SetActive(false);
+        //Image.SetActive(false);
 
-    //    GameManager.Instance.ChangedGameState(GameState.Play);
-    //}
+        //GameManager.Instance.OccurEvent(EventKind.MonsterAppear);
+        //GameManager.Instance.OccurEvent(EventKind.PlayTimeStart);
+
+        //GameManager.Instance.ChangedGameState(GameState.Play);
+    }
 }

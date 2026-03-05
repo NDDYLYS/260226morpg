0e6cd78153838cbdd7b8c66fe501455160f1c0dd
NDDYLYS <nodvic@naver.com>
2026-03-05 20:03:44 +0900

    메뉴 생성, 메세지박스 수정

---
+++ morpg/morpg/Assets/Scripts/Manager/StartManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{
+    public void onClickNewGameButton() 
+    { 
+    }
+
+    public void onClickContinueButton() 
+    { 
+    }
+
+    public void onClickOptionButton() 
+    { 
+    }
+
+    public void onClickExitButton() 
+    { 
+    }
+}

37e029c7c74133337d84ad60dd049107095551ac
NDDYLYS <nodvic@naver.com>
2026-03-21 14:38:01 +0900

    메뉴에서 옵션과 종료 적용

--- morpg/morpg/Assets/Scripts/UI/Page/MenuPage.cs
+++ morpg/morpg/Assets/Scripts/UI/Page/MenuPage.cs
@@ -25,4 +25,39 @@ public class MenuPage : EventProcessor
 
         OnClickClosePageButton();
     }
+
+    public void OnClickOptionButton()
+    {
+        UIPrefabManager.Instance.OptionPageProperty.OnClickOpenPageButton();
+    }
+
+    public void OnClickMenuButton()
+    {
+    }
+
+    public void OnClickTitleButton() 
+    {
+        GameManager.Instance.SaveData = null;
+        GameManager.Instance.MovingScene("1Start");
+    }
+
+    public void OnClickLoadButton()
+    {
+        UIPrefabManager.Instance.SaveLoadPageProperty.OnClickOpenPageButton(SaveLoadEnum.Load);
+    }
+
+    public void OnClickExitButton()
+    {
+        UIPrefabManager.Instance.MessageBoxProperty.OnClickOpenPageButton("GameExit", delegate (MessageBoxClick click)
+        {
+            if (click == MessageBoxClick.Confirm)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+            }
+        });
+    }
 }
